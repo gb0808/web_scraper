b5d14f0a783ebc21b35d25fddddfacc3a8af326c
Gabe Barnard <gbarnard08@gmail.com>
2024-01-20 23:16:36 -0600

    wrote a script to scrap the sccvo website for NSF Award IDs

---
+++ get_awards.php
@@ -0,0 +1,16 @@
+<?php
+
+require 'web_scraper.php';
+
+$award_ids = get_award_id_from_SCCVO();
+$file_name = 'award_ids.csv';
+$f = fopen($file_name, 'w');
+if ($f == false) {
+    die('Error opening the file ' . $filename);
+}
+foreach ($award_ids as $id) {
+    if (is_numeric($id)) {
+        fputcsv($f, [$id]);
+    }
+}
+fclose($f);
